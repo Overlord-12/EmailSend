c01fc10cad3e55a044770866ca21aa06dadc7587
Overlord-12 <danya.argastesv.02@mail.ru>
2021-04-07 22:13:12 +0300

    Добавьте файлы проекта.

---
+++ EmailSend/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net;
+
+namespace EmailSend
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string adress = MailAdr.Text;
+                string topic = MailTopic.Text;
+                MailContent.SelectAll();
+                string body = MailContent.Text;
+                SendMail.Send(topic,body,adress);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+       
+        }
+
+        private void Click(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                string adress = MailAdr.Text;
+                string topic = MailTopic.Text;
+                MailContent.SelectAll();
+                string body = MailContent.Text;
+                SendMail.Send(topic, body, adress);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ EmailSend/SendMail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.Windows;
+
+namespace EmailSend
+{
+    class SendMail
+    {
+        public static void Send(string topic,string body,string toadress)
+        {
+            var fromAddress = new MailAddress($"{MailAdr.Adress}", $"{MailAdr.Name}");
+            var toAddress = new MailAddress( $"{toadress}" , null);
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.mail.ru",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("danya.argastsev.02@mail.ru", "123Danil2"),
+            };
+            using (var message = new MailMessage(Convert.ToString(fromAddress), Convert.ToString
+                (toAddress))
+            {
+                Subject = topic,
+                Body = body
+
+            })
+            {
+                smtp.Send(message);
+            }
+            MessageBox.Show("Сообщение отправлено");
+        }
+    }
+}
